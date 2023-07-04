3b363fcb08dd4e8b941bd31769d364c89b79a029
Adelino Henrique <ade.anjos@gmail.com>
2023-07-04 00:16:48 -0300

    finalAula1

---
+++ src/app/about/components/Trips.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+
+import React, { useEffect, useState } from "react";
+
+
+
+const Trips =  () => {
+
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+      fetch("http://jsonplaceholder.typicode.com/posts")
+        .then((res) => res.json())
+        .then((res) => setData(res));
+    }, []);
+    
+    return (
+        <div>
+        {data.map((i: any) =>(
+            <p key={i.id}>{i.title}</p>
+        ))}
+        </div>
+    );
+};
+
+export default Trips
